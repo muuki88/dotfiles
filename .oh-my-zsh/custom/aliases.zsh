c64d8757db8511a903c8a469273d62fc7a66c117
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2015-05-01 16:05:54 +0200

    Adding  docker-configure and jump commands

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,22 @@
+#!./bin/zsh
 alias python-webserver="python -m SimpleHTTPServer 8888"
+
+# navigation
+
+function j() {
+  git="$HOME/dev/git"
+  case "$1" in
+    g|git )
+      cd $git
+    ;;
+    g-snp )
+      cd "$git/sbt-native-packager"
+    ;;
+    g-a )
+      cd "$git/activator"
+    ;;
+    dl )
+      cd "$HOME/Downloads"
+    ;;
+  esac 
+}
--- .oh-my-zsh/custom/docker.zsh
+++ .oh-my-zsh/custom/docker.zsh
@@ -1,2 +1,3 @@
+alias docker-configure="sudo gpasswd -a ${USER} docker && echo 'restart docker' && sudo service docker restart"
 alias docker-rm-containers="docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm"
 alias docker-rm-images="docker images --no-trunc | grep none | awk '{print $3}' | xargs docker rmi"
