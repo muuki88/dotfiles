b940549d3974925f63e4c188ed819505e6eae549
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2015-08-12 23:11:17 +0200

    Enable zsh plugins

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,20 +3,4 @@ alias python-webserver="python -m SimpleHTTPServer 8888"
 
 # navigation
 
-function j() {
-  git="$HOME/dev/git"
-  case "$1" in
-    g|git )
-      cd $git
-    ;;
-    g-snp )
-      cd "$git/sbt-native-packager"
-    ;;
-    g-a )
-      cd "$git/activator"
-    ;;
-    dl )
-      cd "$HOME/Downloads"
-    ;;
-  esac 
-}
+alias j=jump
--- .zshrc
+++ .zshrc
@@ -47,7 +47,7 @@ ZSH_THEME="afowler"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git git-extras github ubuntu chuck colorize emoji-clock sbt jump)
 
 # User configuration
 
